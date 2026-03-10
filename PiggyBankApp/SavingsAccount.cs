7150b43cc740963691f8b591a4796f15a130bab9
latrisha <l_gomer@yahoo.com>
2026-03-10 08:58:28 -0700

    add savings account class.

---
+++ PiggyBankApp/SavingsAccount.cs
@@ -0,0 +1,12 @@
+
+namespace PiggyBankApp
+{
+    public class SavingsAccount : BankAccount
+    {
+        public SavingsAccount(string accountHolder, decimal startingBalance)
+            : base (accountHolder, startingBalance)
+        {
+            
+        }
+    }
+}
